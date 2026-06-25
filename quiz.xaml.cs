5e30fb4cc8a22755dd55b007b02f54dfaca5e248
hlulani <st10474357@rcconnect.edu.za>
2026-06-25 15:25:35 +0200

    creation of start of quiz questions

--- quiz.xaml.cs
+++ quiz.xaml.cs
@@ -25,10 +25,55 @@ namespace Chatbot
         public quiz()
         {
             InitializeComponent();
-            answeButtons = new List<Button>();  //Listing all the buttons we will be using in this instance 
+            answerButtons = new List<Button>();
+
+            LoadQuestions();//calling the questions method and displaying all the questions
+            ShuffleQuestions();//mix questions areound for senitent user interactions
+            ShowQuestion();//display all the questions
+
+
+
+
+            
+            //Listing all the buttons we will be using in this instance 
         }
 
 
+        private void LoadQuestion()
+
+        {
+            questions = new List<QuizQuestion>
+            {
+
+                new QuizQuestion(
+                    "What does 2FA stand for?",//displayed question
+                    new[] { "Two-Factor Authentication", "Two-File Access", "Trusted Firewall Application", "Two-Frequency Algorithm" },
+                    0,
+               "Correct , two factor authentication stands for that "
+
+
+
+
+
+
+        }
+
+        //getters for retrieving and loading all the quiz questions 
+        public class QuizQuestion
+        {
+            public string Question { get; }
+            public string[] Options { get; }
+            public int CorrectIndex { get; }
+            public string Feedback { get; }
+
+            public QuizQuestion(string question, string[] options, int correctIndex, string feedback)
+            {
+                Question = question;
+                Options = options;
+                CorrectIndex = correctIndex;
+                Feedback = feedback;
+            }
+        }
 
 
     }
